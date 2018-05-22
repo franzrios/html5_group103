120b033e42476f995607a156c2c366c281ecbe3c
Erik Porroa <erik@porroa.com>
2018-05-21 19:08:39 -0500

    clase 18 + 19

---
+++ modulo03/clase 18/condicional/if-else.js
@@ -0,0 +1,59 @@
+/*
+== : IGUAL SIMPLE
+=== : IGUAL ESTRICTO
+!= : NO ES IGUAL SIMPLE
+!== : NO ES IGUAL ESTRICTO
+*/
+
+// var password = 12345;
+// var esValido = password == 1234;
+
+// if(esValido){
+//   alert('Bienvenido');
+// } else {
+//   alert("Acceso denegado");
+// }
+
+
+var password = 'bolivia';
+
+if(password === 'peru'){
+  alert('Bienvenido, Perú');
+} else if(password === 'colombia'){
+  alert('Bienvenido, Colombia');
+}else if(password === 'argentina'){
+  alert('Bienvenido, Argentina');
+}else{
+  alert('Acceso Denegado');
+}
+
+/*
+&& = AND = Y lógico 
+|| = OR = O Lógico
+*/
+var palabra = 'peru';
+
+if(palabra != 'peru' && palabra != 'españa'){
+	// ...
+}
+
+if(palabra == 'peru' || palabra == 'españa'){
+	// ...
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ modulo03/clase 19/arrays-objects/friends_list.js
@@ -0,0 +1,3 @@
+var anna = { name: "Anna", age: 11, luckyNumbers: [2, 4, 8, 16] };
+var dave = { name: "Dave", age: 5, luckyNumbers: [3, 9, 40] };
+var kate = { name: "Kate", age: 9, luckyNumbers: [1, 2, 3] };
