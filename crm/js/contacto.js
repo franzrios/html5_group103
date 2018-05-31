50e8584a69dda23596efd2d3b4e86572b02652e2
Erik Porroa <erik@porroa.com>
2018-05-30 22:17:37 -0500

    clase 23

--- crm/js/contacto.js
+++ crm/js/contacto.js
@@ -1,5 +1,61 @@
-$(function () {
+var contactos = [];
 
+function listarContactos(){
+	var $listadoTable = $('#listado-contactos');
+	var tableVacio = $listadoTable.find('tbody').html();
+
+	if(contactos.length > 0){
+		$listadoTable.find('tbody').empty();
+	}
+
+	for (var i = 0; i < contactos.length; i++) {
+		var contacto = contactos[i];
+
+		var $tr = $('<tr></tr>');
+		$tr.append('<td>' + contacto.nombre + '</td>');
+		$tr.append('<td>' + contacto.telefono + '</td>');
+		$tr.append('<td>' + contacto.email + '</td>');
+		$tr.append('<td>' + contacto.compania + '</td>');
+		$tr.append('<td>' + contacto.ultimocontacto + '</td>');
+		$tr.append('<td>' + '<button data-ind="'+ i +'">Editar</editar> <button data-ind="'+ i +'">Eliminar</editar>' + '</td>');
+
+		$tr.find('button').on('click', function(event){
+			var $btnClick = $(event.currentTarget);
+			var index = $btnClick.data('ind');
+			var action = event.currentTarget.innerText;
+
+			if(action == 'Editar'){
+				$('.detalle').slideDown();
+				var contacto = contactos[index];
+				var inputs = Object.keys(contacto);
+
+				for (var i = 0; i < inputs.length; i++) {
+					$('#' + inputs[i]).val(contacto[ inputs[i] ]);
+				}
+
+			}else if(action == "Eliminar"){
+				contactos.splice(index, 1);
+				listarContactos();
+			}
+		});
+
+		$listadoTable.find('tbody').append($tr);
+	}
+}
+
+
+$( function () {
+	$.get("datos/contactos.json", function(resp){
+		if(resp.length > 0){
+			contactos = resp;
+		}
+		listarContactos();
+	});
+
+	$('.detalle').slideUp();
+	$('.btn-nuevo').click(function(){
+		$('.detalle').slideToggle();
+	});
 
 	$('form').on('submit', function(event){
 		event.preventDefault();
@@ -12,13 +68,8 @@ $(function () {
 			$('#' + inputs[i].name).val('');
 		}
 
-		var $tr = $('<tr></tr>');
-		$tr.append('<td>' + contacto.nombre + '</td>');
-		$tr.append('<td>' + contacto.telefono + '</td>');
-		$tr.append('<td>' + contacto.email + '</td>');
-		$tr.append('<td>' + contacto.compania + '</td>');
-		$tr.append('<td>' + contacto.ultimocontacto + '</td>');
-
-		$tr.appendTo('table tbody');
+		contactos.push(contacto);
+		listarContactos();
 	})
-});
+
+} );
